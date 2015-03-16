32887b0db559ffa49c3d432ebe9a4fb1011b75df
Neodyblue <sieramsn@gmail.com>
2015-03-16 17:58:20 +0000

    Added basic opcode manager

---
+++ include/interpreter/opcode_manager.hh
@@ -0,0 +1,22 @@
+#ifndef OPCODE_MANAGER_HH
+# define OPCODE_MANAGER_HH
+
+# define MAX_OPCODE 256
+
+class OpcodeManager
+{
+  public:
+    OpcodeManager();
+    ~OpcodeManager();
+
+    typedef void (*op_handler_type)(void); //FIXME
+
+    void register_opcode_handler(unsigned char opcode, op_handler_type handler);
+
+  private:
+    static void handler_placeholder(void); //FIXME
+
+    op_handler_type handlers[MAX_OPCODE];
+};
+
+#endif /* end of include guard: OPCODE_MANAGER_HH */
---
+++ src/interpreter/opcode_manager.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#include "interpreter/opcode_manager.hh"
+
+OpcodeManager::OpcodeManager()
+{
+  for (unsigned i = 0; i < MAX_OPCODE; ++i)
+    handlers[i] = handler_placeholder;
+}
+
+void OpcodeManager::handler_placeholder(void)
+{
+  std::cerr << "Unknown opcode" << std::endl;
+}
+
+void OpcodeManager::register_opcode_handler(unsigned char opcode,
+                                       OpcodeManager::op_handler_type handler)
+{
+  handlers[opcode] = handler;
+}

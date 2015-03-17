4db11f15232fbd778e096b6ffa236fd4641f59e7
Neodyblue <sieramsn@gmail.com>
2015-03-17 18:19:29 +0000

    Added basic CPU

---
+++ include/cpu/cpu.hh
@@ -0,0 +1,33 @@
+#ifndef CPU_HH
+# define CPU_HH
+
+# include <vector>
+# include <stack>
+
+# include "cpu/registers.hh"
+# include "interpreter/opcode_manager.hh"
+
+namespace cpu
+{
+  class CPU
+  {
+    public:
+      CPU(unsigned gen_reg,
+          unsigned char* code,
+          unsigned entry_point,
+          interpreter::OpcodeManager op_manager);
+      ~CPU();
+
+      void run();
+      inline unsigned char fetch_opcode();
+
+      Registers regs;
+
+    private:
+      std::stack<long long> stack_;
+      unsigned char* bytecode_;
+      interpreter::OpcodeManager op_manager_;
+  };
+}
+
+#endif /* end of include guard: CPU_HH */
---
+++ src/cpu/cpu.cc
@@ -0,0 +1,24 @@
+#include "cpu/cpu.hh"
+
+using namespace cpu;
+
+CPU::CPU(unsigned gen_reg,
+         unsigned char* code,
+         unsigned entry_point,
+         interpreter::OpcodeManager op_manager)
+  : regs(gen_reg)
+  , bytecode_(code)
+  , op_manager_(op_manager)
+{
+  regs.PC = entry_point;
+}
+
+inline unsigned char CPU::fetch_opcode()
+{
+  return bytecode_[regs.PC++];
+}
+
+void CPU::run()
+{
+  //TODO: do something
+}

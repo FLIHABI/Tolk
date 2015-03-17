cfeb9b4defae9446aaf466619a77a2fcd08fb9de
Neodyblue <sieramsn@gmail.com>
2015-03-17 17:52:28 +0000

    Added basic register class

---
+++ include/cpu/registers.hh
@@ -0,0 +1,19 @@
+#ifndef REGISTERS_HH
+# define REGISTERS_HH
+
+# include <vector>
+
+namespace cpu
+{
+  class Registers
+  {
+    public:
+      Registers(unsigned  gen_reg);
+      ~Registers();
+
+      unsigned PC;
+      std::vector<long long> greg;
+  };
+}
+
+#endif /* end of include guard: REGISTERS_HH */
---
+++ src/cpu/registers.cc
@@ -0,0 +1,7 @@
+#include "cpu/registers.hh"
+
+using namespace cpu;
+
+Registers::Registers(unsigned gen_reg)
+  : greg(gen_reg)
+{}

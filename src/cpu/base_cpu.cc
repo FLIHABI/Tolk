5fc1a97593d8117c8846960fb91065ce740bd556
Neodyblue <sieramsn@gmail.com>
2015-03-22 05:24:36 +0000

    Added default initialization for registers struct

--- src/cpu/base_cpu.cc
+++ src/cpu/base_cpu.cc
@@ -4,7 +4,7 @@ using namespace cpu;
 
 BaseCPU::BaseCPU(unsigned gen_reg, unsigned char* code, unsigned entry_point)
   : regs(gen_reg)
-  , flags{}
+  , flags()
   , bytecode_(code)
 {
   regs.PC = entry_point;

393d4ca89995f5ced90932f09dd550004328e308
Neodyblue <sieramsn@gmail.com>
2015-04-07 16:27:40 +0100

    Added basic main

--- src/main.cc
+++ src/main.cc
@@ -1,27 +1,13 @@
-#include <iostream>
-
 #include "cpu/cpu.hh"
-#include "interpreter/opcode_manager.hh"
-
-bool halt_handler(cpu::BaseCPU& cpu)
-{
-  std::cout << "Halting ..." << std::endl;
-  return false;
-}
-
-bool print_pc_handler(cpu::BaseCPU& cpu)
-{
-  std::cout << "Current PC: " << cpu.regs.PC << std::endl;
-  return true;
-}
+#include "loader.hh"
 
 int main()
 {
   interpreter::OpcodeManager opm;
-  opm.register_opcode_handler('H', halt_handler);
-  opm.register_opcode_handler('P', print_pc_handler);
+  Loader::get_instance().init_handlers_manager(opm);
 
-  cpu::CPU cpu(1, (unsigned char*)"PPPH", 0, opm);
+  cpu::CPU cpu(1, (unsigned char*)"", 0, opm);
   cpu.run();
+
   return 0;
 }

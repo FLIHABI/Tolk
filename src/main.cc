b611f5d2004e6752af5458c1a6ca2de0d409d986
Neodyblue <sieramsn@gmail.com>
2015-04-21 11:32:36 +0200

    Added input file support

--- src/main.cc
+++ src/main.cc
@@ -1,13 +1,54 @@
+#include <iostream>
+#include <fstream>
+
 #include "cpu/cpu.hh"
 #include "loader.hh"
 
-int main()
+static char* read_input_file(char* filename)
 {
+  std::ifstream is(filename);
+
+  if (!is)
+  {
+    std::cerr << "Cannot open file '" << filename << "'" << std::endl;
+    std::exit(1);
+  }
+
+  is.seekg(0, is.end);
+  int size = is.tellg();
+  is.seekg(0, is.beg);
+
+  char* bytecode = new char[size];
+
+  if (!is.read(bytecode, size))
+  {
+    std::cerr << "Cannot read file" << std::endl;
+    is.close();
+    std::exit(1);
+  }
+
+  is.close();
+
+  return bytecode;
+}
+
+int main(int argc, char* argv[])
+{
+  if (argc != 2)
+  {
+    std::cerr << "Usage: " << argv[0] << " <input>" << std::endl;
+    return 1;
+  }
+
   interpreter::OpcodeManager opm;
   Loader::get_instance().init_handlers_manager(opm);
 
-  cpu::CPU cpu(1, (unsigned char*)"", 0, opm);
+  char* bytecode = read_input_file(argv[1]);
+
+  cpu::CPU cpu(1, bytecode, 0, opm);
   cpu.run();
+
+  delete[] bytecode;
 
   return 0;
 }

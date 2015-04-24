29b15c9ecffc859e16beda28b531c279fe8aa2bf
Neodyblue <sieramsn@gmail.com>
2015-04-24 10:56:44 +0200

    Added environment class

---
+++ include/environment.hh
@@ -0,0 +1,28 @@
+#ifndef ENVIRONMENT_HH
+# define ENVIRONMENT_HH
+
+# include "cpu/base_cpu.hh"
+
+class Environment
+{
+  public:
+    Environment(cpu::BaseCPU& cpu);
+
+    inline int64_t stack_pop()
+    {
+      int64_t top = stack.top();
+      stack.pop();
+
+      return top;
+    }
+
+    inline void stack_push(int64_t value)
+    {
+      stack.push(value);
+    }
+
+    cpu::BaseCPU& cpu;
+    std::stack<int64_t> stack;
+};
+
+#endif /* end of include guard: ENVIRONMENT_HH */
---
+++ src/environment.cc
@@ -0,0 +1,5 @@
+#include "environment.hh"
+
+Environment::Environment(cpu::BaseCPU& cpu)
+  : cpu(cpu)
+{}

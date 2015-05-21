3ce12e8a22327d4403402ca49907a163775a67d3
Neodyblue <sieramsn@gmail.com>
2015-05-21 12:48:57 +0200

    Added prototypes of objetct, jumps and all handlers

--- include/interpreter/handlers/handlers.hh
+++ include/interpreter/handlers/handlers.hh
@@ -17,6 +17,23 @@ namespace interpreter
     bool mul_handler(Environment& env);
     bool div_handler(Environment& env);
     bool mod_handler(Environment& env);
+    //TODO:
+    bool call_handler(Environment& env);
+    bool callr_handler(Environment& env);
+    bool save_handler(Environment& env);
+    bool restore_handler(Environment& env);
+    bool create_handler(Environment& env);
+    bool delete_handler(Environment& env);
+    bool read_handler(Environment& env);
+    bool write_handler(Environment& env);
+    bool ret_handler(Environment& env);
+    bool jmp_handler(Environment& env);
+    bool je_handler(Environment& env);
+    bool jl_handler(Environment& env);
+    bool jg_handler(Environment& env);
+    bool jne_handler(Environment& env);
+    bool jle_handler(Environment& env);
+    bool jge_handler(Environment& env);
   }
 }
 

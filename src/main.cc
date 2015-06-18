0eb24232d3acfb625334c1042bfcff1a3f7da033
Neodyblue <sieramsn@gmail.com>
2015-06-18 12:33:56 +0200

    Using NS in main (only for server mode)

--- src/main.cc
+++ src/main.cc
@@ -28,64 +28,64 @@ int main(int argc, char* argv[])
   }
   catch(std::exception& e)
   {
+    std::cerr << e.what() << std::endl;
     print_usage(argv[0]);
     return 1;
   }
 
   interpreter::OpcodeManager opm;
-  ressource::RessourceManager rm;
+  network::Service net_svc(args.mode);
+  ressource::RessourceManager rm(net_svc);
 
   Loader::get_instance().init_handlers_manager(opm);
 
-  //TODO: use mode
-  std::shared_ptr<Server> server;
-  if (!rm.load_file(args.filename))
-  {
-    std::cerr << "Cannot open file '" << args.filename << "'" << std::endl;
-    return 1;
-  }
-
-  //TODO: Move server to RM
-  //rm.set_server(server);
   //TODO: read gen_reg from file
-
-  if (server)
+  if (args.mode == network::SERVER)
   {
+    if (!rm.load_file(args.filename))
+    {
+      std::cerr << "Cannot open file '" << args.filename << "'" << std::endl;
+      return 1;
+    }
+
+    net_svc.start();
+
     cpu::BaseCPU cpu(4096, rm.get_bytecode(), rm.get_entry_point(), opm);
     Environment env(cpu, rm);
+
     env.run();
   }
-  else
-  {
-    //TODO clean ressource manager between each call
-    while (1)
-    {
-      Slave s;
-      std::string b = s.getBytecode();
-      std::cout << "Receive: " << b.size() << std::endl;
-      std::vector<uint64_t> result(b.size() / 8);
-      std::copy((uint64_t*)&b[0], ((uint64_t*)&b[0]) + b.size() / 8, &result[0]);
-      std::cout << "Receive " << result.size() << std::endl;
-      std::cout << "Vector has " << result[1] << " elts" << std::endl;
-      auto p = rm.deserialize_call(result);
-      //TODO call env
-      cpu::BaseCPU cpu(4096,
-          rm.get_bytecode(),
-          rm.get_tolk_file()->get_functable().get(p.first).offset,
-          opm);
-      Environment env(cpu, rm);
-      for (unsigned i = 0; i < p.second.size(); i++)
-      {
-        env.stack_push(p.second[i]);
-      }
-      env.run();
-      uint64_t ret = env.cpu.regs.greg[0];
-      result = rm.serialize_return(p.first, ret);
-      b = std::string((char*)&result[0], result.size() * 8);
-      std::cout << "Answering" << std::endl;
-      s.send_bytecode(b);
-    }
-  }
+  //else
+  //{
+  //    //TODO clean ressource manager between each call
+//    while (1)
+//    {
+//      Slave s;
+//      std::string b = s.getBytecode();
+//      std::cout << "Receive: " << b.size() << std::endl;
+//      std::vector<uint64_t> result(b.size() / 8);
+//      std::copy((uint64_t*)&b[0], ((uint64_t*)&b[0]) + b.size() / 8, &result[0]);
+//      std::cout << "Receive " << result.size() << std::endl;
+//      std::cout << "Vector has " << result[1] << " elts" << std::endl;
+//      auto p = rm.deserialize_call(result);
+//      //TODO call env
+//      cpu::BaseCPU cpu(4096,
+//          rm.get_bytecode(),
+//          rm.get_tolk_file()->get_functable().get(p.first).offset,
+//          opm);
+//      Environment env(cpu, rm);
+//      for (unsigned i = 0; i < p.second.size(); i++)
+//      {
+//        env.stack_push(p.second[i]);
+//      }
+//      env.run();
+//      uint64_t ret = env.cpu.regs.greg[0];
+//      result = rm.serialize_return(p.first, ret);
+//      b = std::string((char*)&result[0], result.size() * 8);
+//      std::cout << "Answering" << std::endl;
+//      s.send_bytecode(b);
+//    }
+//  }
 
 
   return 0;
